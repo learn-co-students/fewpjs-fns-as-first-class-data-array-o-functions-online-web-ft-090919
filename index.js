0b2a241071fad2fbeebb2fe3c179b0a2055d452d
Doug Kintop <dkintop@hotmail.com>
2019-12-12 15:24:44 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function wakeDog(dog_name, dog_breed) {
+  return `Wake ${dog_name} the ${dog_breed}`;
+}
+
+function leashDog() {
+  return `Leash Boo Radley the Pibble`;
+}
+
+function walkToPark() {
+  return `Walk to the park with Bunny the Labrador`;
+}
+
+function throwFrisbee() {
+  return `Throw the frisbee for Mary J. Blige the Papillon`;
+}
+
+function walkHome() {
+  return "Walk home with Santa Paws the Husky";
+}
+
+function unleashDog() {
+  return "Unleash Lauren the Golden Retriever";
+}
+
+let routine = [wakeDog
+    leashDog()
+    walkToPark()
+    throwFrisbee()
+    walkHome()
+    unleashDog()];
