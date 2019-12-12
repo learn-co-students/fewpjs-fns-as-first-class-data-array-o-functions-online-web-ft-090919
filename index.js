e4119744e2ce8fc1be596d7b98b6e51e1bc2490e
Doug Kintop <dkintop@hotmail.com>
2019-12-12 15:29:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-function wakeDog(dog_name, dog_breed) {
-  return `Wake ${dog_name} the ${dog_breed}`;
-}
+var wakeDog = function(dogName = "Byron", dogBreed = "poodle") {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+};
 
-function leashDog() {
-  return `Leash Boo Radley the Pibble`;
-}
+var leashDog = function(dogName = "Byron", dogBreed = "poodle") {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`;
+};
 
-function walkToPark() {
-  return `Walk to the park with Bunny the Labrador`;
-}
+var walkToPark = function(dogName = "Byron", dogBreed = "poodle") {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`;
+};
 
-function throwFrisbee() {
-  return `Throw the frisbee for Mary J. Blige the Papillon`;
-}
+var throwFrisbee = function(dogName = "Byron", dogBreed = "poodle") {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+};
 
-function walkHome() {
-  return "Walk home with Santa Paws the Husky";
-}
+var walkHome = function(dogName = "Byron", dogBreed = "poodle") {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return `Walk home with ${dogName} the ${dogBreed}`;
+};
 
-function unleashDog() {
-  return "Unleash Lauren the Golden Retriever";
-}
+var unleashDog = function(dogName = "Byron", dogBreed = "poodle") {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
+};
 
-let routine = [wakeDog
-    leashDog()
-    walkToPark()
-    throwFrisbee()
-    walkHome()
-    unleashDog()];
+const routine = [
+  wakeDog,
+  leashDog,
+  walkToPark,
+  throwFrisbee,
+  walkHome,
+  unleashDog
+];
+
+function exerciseDog(dog, breed) {
+  return routine.map(fn => fn(dog, breed));
+}
